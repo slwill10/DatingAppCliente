e638b1d0102e70b8605a4539255c5c1705c6fc1a
Polyana Cunha <polyana.cunha10@gmail.com>
2023-12-28 12:58:45 -0300

    Fixing 'welcome user' drop down

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule, 
-    FormsModule
+    FormsModule, 
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
