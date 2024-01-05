8a91a3583d0a47efaa4ed3ef05428b528649ce87
Willian Santos <wsantosdelima44@gmail.com>
2024-01-05 13:51:03 -0300

    Adding an agular route guard

---
+++ src/app/_guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+import { Toast, ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree 
+  {
+ 
+    return this.accountService.currentUser$.pipe(
+      map(user => {
+        if(user) return true;
+        else {
+          this.toastr.error('you shall not pass!');
+          return false;
+        }
+      })
+    )   
+  }
+}
